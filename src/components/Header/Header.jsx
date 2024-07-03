aca6b39dac85108aa2f2c3a8039595226188e4a7
Asif Khan <sparkleasif@gmail.com>
2024-07-03 14:52:01 +0600

    initial dataload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+      <nav className='flex text-xl space-x-5  '>
+        <Link to={"/"} >Home</Link> 
+        <Link to={"/review"} >Order Review</Link> 
+        <Link to={"/about"} >About</Link> 
+        <Link to={"/contact"} >Contact</Link> 
+      </nav>
+    );
+};
+
+export default Header;
